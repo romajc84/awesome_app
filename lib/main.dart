c158aab1ecac0ceee8f919551c11cda870703e66
romajc84 <joeyroman84@gmail.com>
2021-06-05 12:14:00 -0400

    ch2 complete

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,42 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Awesome App"),
+      appBar: AppBar(
+        title: Text("Awesome App"),
       ),
       body: Container(
-        child: Text("Hi Flutter"),
+        height: 100,
+        color: Colors.teal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          color: Colors.red,
+          alignment: Alignment.center,
+          
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          color: Colors.yellow,
+          alignment: Alignment.center,
+          
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          color: Colors.green,
+          alignment: Alignment.center,
+          
+        ),
+          ],
+        ),
       ),
     );
   }
