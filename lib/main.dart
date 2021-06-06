f1d25caaacf99cfa365a2a5b48594a3fc0bafbfd
romajc84 <joeyroman84@gmail.com>
2021-06-06 09:30:30 -0400

    ch4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,21 @@
+import 'package:awesome_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: HomePage(),
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-    ),
+    MyApp(),
   );
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  var myText = "Change My Name";
-  TextEditingController _nameController = TextEditingController();
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
-
-
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text("Awesome App"),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: Card(
-                child: Column(
-              children: [
-                Image.asset(
-                  "assets/macimg.jpg",
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  myText,
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: TextField(
-                    controller: _nameController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Enter Some Text",
-                      labelText: "Name",
-                    ),
-                  ),
-                ),
-              ],
-            )),
-          ),
-        ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: const EdgeInsets.all(0),
-          children: [
-            UserAccountsDrawerHeader(
-                accountName: Text("Joey Roman"),
-                accountEmail: Text("joeyroman84@gmail.com"),
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      "https://images.unsplash.com/photo-1497485692312-a26e1cc30f1d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGd1eXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1296&q=60"),
-                )),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text("Joey Roman"),
-              subtitle: Text("Developer"),
-              trailing: Icon(Icons.edit),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.email),
-              title: Text("Email"),
-              subtitle: Text("joeyroman@gmail.com"),
-              trailing: Icon(Icons.edit),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          myText = _nameController.text;
-          setState(() {
-            
-          });
-        },
-        child: Icon(Icons.send),
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
       ),
     );
   }
