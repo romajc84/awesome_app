c2e873bdb80cd6fd606f3992b97f6496a64561c4
romajc84 <joeyroman84@gmail.com>
2021-06-06 08:29:31 -0400

    ch3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -11,48 +11,112 @@ void main() {
   );
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var myText = "Change My Name";
+  TextEditingController _nameController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        height: 100,
-        color: Colors.teal,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Card(
+                child: Column(
+              children: [
+                Image.asset(
+                  "assets/macimg.jpg",
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  myText,
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Enter Some Text",
+                      labelText: "Name",
+                    ),
+                  ),
+                ),
+              ],
+            )),
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
           children: [
-            Container(
-          padding: const EdgeInsets.all(8),
-          width: 100,
-          height: 100,
-          color: Colors.red,
-          alignment: Alignment.center,
-          
-        ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          width: 100,
-          height: 100,
-          color: Colors.yellow,
-          alignment: Alignment.center,
-          
-        ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          width: 100,
-          height: 100,
-          color: Colors.green,
-          alignment: Alignment.center,
-          
-        ),
+            UserAccountsDrawerHeader(
+                accountName: Text("Joey Roman"),
+                accountEmail: Text("joeyroman84@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/photo-1497485692312-a26e1cc30f1d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGd1eXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1296&q=60"),
+                )),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Joey Roman"),
+              subtitle: Text("Developer"),
+              trailing: Icon(Icons.edit),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("joeyroman@gmail.com"),
+              trailing: Icon(Icons.edit),
+            ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          myText = _nameController.text;
+          setState(() {
+            
+          });
+        },
+        child: Icon(Icons.send),
       ),
     );
   }
